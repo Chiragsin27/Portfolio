d97d0020a505a9c407804d83b07314703a69e26b
Chiragsin27 <chirag.2023ug1097@iiitranchi.ac.in>
2025-01-02 17:49:06 +0530

    Init

---
+++ constants/index.ts
@@ -0,0 +1,125 @@
+import { RxHome, RxPerson, RxDashboard, RxClipboard } from "react-icons/rx";
+
+export const SkillData = [
+  {
+    name: "Html 5",
+    Image: "/html.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "Css",
+    Image: "/css.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "JavaScript",
+    Image: "/js.png",
+    width: 65,
+    height: 65,
+  },
+  {
+    name: "Tailwind Css",
+    Image: "/tailwind.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "React",
+    Image: "/react.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "Redux",
+    Image: "/redux.png",
+    width: 80,
+    height: 80,
+  },
+
+  {
+    name: "TypeScript",
+    Image: "/ts.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "Next js 13",
+    Image: "/next.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "Framer Motion",
+    Image: "/framer.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "Node js",
+    Image: "/node-js.png",
+    width: 80,
+    height: 80,
+  },
+];
+
+export const Socials = [
+  {
+    name: "Discord",
+    src: "/instagram.svg",
+  },
+  {
+    name: "Facebook",
+    src: "/facebook.svg",
+  },
+  {
+    name: "Instagram",
+    src: "/discord.svg",
+  },
+];
+export const Projects = [
+  {
+    title: "PregaHelp Website",
+    text: "PregaHelp is a Pregnancy Risk calculator based on BMI and other health related factors.",
+    src: "/NextWebsite.png",
+  },
+  {
+    title: "Hospital Queuing Website",
+    text: "Frontend of a Queue management website",
+    src: "/SpaceWebsite.png",
+  },
+  {
+    title: "Nextjs Portfolio",
+    text: "Portfolio made using Next.js and TyepeScript.",
+    src: "/WebPortfolio.png",
+  },
+  {
+    title: "Student Management Website",
+    text: "Frontend of the web version of a student management app.",
+    src: "/Matrix.png",
+  },
+];
+
+export const NavLinks = [
+  {
+    name: "/",
+    icon: RxHome,
+    link: "/",
+  },
+  {
+    name: "/my-skills",
+    icon: RxPerson,
+    link: "/my-skills",
+  },
+  {
+    name: "/my-projects",
+    icon: RxDashboard,
+    link: "/my-projects",
+  },
+  {
+    name: "/contact-me",
+    icon: RxClipboard,
+    link: "/contact-me",
+  },
+];
