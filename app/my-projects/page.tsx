7c1247d36f74e91114c75ea5678affe2454e4527
Chiragsin27 <155078974+Chiragsin27@users.noreply.github.com>
2025-01-02 18:31:25 +0530

    Update page.tsx

--- app/my-projects/page.tsx
+++ app/my-projects/page.tsx
@@ -10,7 +10,7 @@ const Page = () => {
     style={{backgroundImage: "url(/mountains.jpg)"}}
      className='w-screen h-screen flex items-center justify-center bg-center bg-cover'>
       <div className='grid grid-cols-2 gap-5 max-w-[90%] max-h-[90%]'>
-        {Projects.map((project: { title: any; text: any; src: any }, index: any) => (
+        {Projects.map((project, index) => (
           <ProjectCard
               key={index}
               title={project.title}
